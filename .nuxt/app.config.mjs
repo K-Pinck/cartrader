5c6eabab43a447c1d397f7196f3969a018c4315a
K-Pinck <meefis6@gmail.com>
2023-08-15 15:37:54 -0500

    project setup

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/keithpinckard/Work/cartrader/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/keithpinckard/Work/cartrader/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
