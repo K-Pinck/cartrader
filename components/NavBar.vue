05fcf3967ddbb739541a9a54639d6f6aadb1a2e7
K-Pinck <72100851+K-Pinck@users.noreply.github.com>
2023-08-18 10:25:23 -0500

    components created

---
+++ components/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- Nav Bar -->
+  <section class="navbar-section">
+    <header
+      class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
+    >
+      <a href="#" class="text-3xl font-mono">CarTrader</a>
+    </header>
+  </section>
+  <!-- /Nav Bar -->
+</template>
