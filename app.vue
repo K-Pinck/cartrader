f4def68a89ed194cea20f42827d295c8f5982746
K-Pinck <72100851+K-Pinck@users.noreply.github.com>
2023-08-18 09:46:36 -0500

    car deatails page html created

--- app.vue
+++ app.vue
@@ -1,14 +1,5 @@
 <template>
   <div>
-    <!-- Nav Bar -->
-    <section class="navbar-section">
-      <header
-        class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
-      >
-        <a href="#" class="text-3xl font-mono">CarTrader</a>
-      </header>
-    </section>
-    <!-- /Nav Bar -->
-
+    
   </div>
 </template>
