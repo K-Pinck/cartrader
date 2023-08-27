643c82a6883962ba7fafc09ceb8ccc1cf293cddd
K-Pinck <meefis6@gmail.com>
2023-08-27 15:44:03 -0500

    site update

--- app.vue
+++ app.vue
@@ -1,54 +1,5 @@
 <template>
   <div>
-    <!-- Nav Bar -->
-    <section class="navbar-section">
-      <header
-        class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
-      >
-        <a href="#" class="text-3xl font-mono">CarTrader</a>
-      </header>
-    </section>
-    <!-- /Nav Bar -->
-
-    <!-- Car details page -->
-    <div>
-      <!-- Car hero -->
-      <div
-        class="cars-content-wrap mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/4"
-      >
-        <div class="mt-10">
-          <img
-            class="w-full"
-            src="https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-            alt=""
-          />
-          <h1 class="mt-10 text-4xl">Volvo 3XT</h1>
-          <div
-            class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between"
-          >
-            <div class="flex">
-              <p class="mr-2">5 seats</p>
-              <p class="mr-2">|</p>
-              <p class="mr-2">67,444 miles</p>
-            </div>
-            <div class="font-bold text-2xl">$25,555</div>
-          </div>
-        </div>
-        <!-- Car attr -->
-      <div class="mr-10 mt-5 border-b pb-5">
-        <div class="flex items-center text-lg mt-2">
-          <p class="rounded text-lime-800 mr-3">
-            <img class="w-4 h-4" src="https://e7.pngegg.com/pngimages/920/543/png-clipart-black-and-white-finger-font-black-checkmark-white-text-thumbnail.png" alt="">
-          </p>
-          <p>Leather Interior</p>
-        </div>
-      </div>
-      <!-- /Car attr -->
-      </div>
-      <!-- /Car hero -->
-
-     
-    </div>
-    <!-- /Car details page -->
+   
   </div>
 </template>
