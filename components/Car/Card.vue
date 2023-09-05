ac5bece1d7dc63410720bb9aae6cbdb432a71039
K-Pinck <meefis6@gmail.com>
2023-09-05 15:31:45 -0500

    car card component created

---
+++ components/Car/Card.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- Car cards -->
+    <div
+          class="car-card shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+        >
+          <div class="flex h-full">
+            <img
+              src="https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+              alt=""
+              class="w-[300px] h-full"
+            />
+            <div class="p-4 flex flex-col items-start">
+              <div>
+                <h1 class="text-2xl text-blue-700"></h1>
+                Volvo XC40
+              </div>
+              <p class="text-gray-700">lorum ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <h1 class="mt-auto text-xl">$39,555</h1>
+            </div>
+            
+          </div>
+        </div>
+        <!-- /Car cards -->
+</template>
--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -1,3 +1,15 @@
 <template>
-    <div>City and Make</div>
+    <div>
+        <NavBar/>
+        <!-- Cars page -->
+    <div
+      class="cars-content-wrap mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/4"
+    >
+      <div class="mt-32 flex">
+        <CarSidebar/>
+        <CarCard/>        
+      </div>
+    </div>
+    <!-- /Cars page -->
+    </div>
 </template>
