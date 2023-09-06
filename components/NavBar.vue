458498d3f8c3cc47715d16200aea55f658c5e3fc
K-Pinck <meefis6@gmail.com>
2023-09-06 15:29:04 -0500

    added nuxtlink to nav bar logo

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,7 +4,7 @@
     <header
       class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
     >
-      <a href="#" class="text-3xl font-mono">CarTrader</a>
+      <NuxtLink to="/" class="text-3xl font-mono">CarTrader</NuxtLink>
     </header>
   </section>
   <!-- /Nav Bar -->
